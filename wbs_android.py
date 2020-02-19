43a6b1971579497348d254b13f7506fdec9a1fb2
doron3199 <doron3199@gmail.com>
2020-02-19 15:41:15 +0200

    small improvments

--- wbs_android.py
+++ wbs_android.py
@@ -10,11 +10,9 @@ def change():
 def show():
     adbCmd = ['adb', 'shell', 'screenrecord', '--output-format=h264', '-']
     stream = sp.Popen(adbCmd, stdout=sp.PIPE, universal_newlines=True)
-    ffmpegCmd = ['ffmpeg', '-i', '-', '-f', 'rawvideo', '-vcodec', 'bmp', '-vf', 'fps=10', '-']
+    ffmpegCmd = ['ffmpeg', '-i', '-', '-f', 'rawvideo', '-vcodec', 'bmp', '-vf', 'fps=7', '-']
     ffmpeg = sp.Popen(ffmpegCmd, stdin=stream.stdout, stdout=sp.PIPE)
-    cv.namedWindow('image', flags=cv.WINDOW_NORMAL)
-    cv.resizeWindow('image', 1800, 1100)
-    cv.createTrackbar("filter", "image", 0, 1, change)
+
 
     while True:
         fileSizeBytes = ffmpeg.stdout.read(6)
@@ -32,15 +30,16 @@ def show():
         for i in range(4):
             fileSize += fileSizeBytes[i + 2] * 256 ** i
         bmpData = fileSizeBytes + ffmpeg.stdout.read(fileSize - 6)
-        org = cv.imdecode(np.fromstring(bmpData, np.uint8), 0)
-
+        org = cv.imdecode(np.fromstring(bmpData, np.uint8), 1)
+        image = cv.cvtColor(org, cv.COLOR_BGR2GRAY)
         kernel = np.ones((5, 5), np.float32) / 25
-        image = cv.filter2D(org, -1, kernel)
+        image = cv.filter2D(image, -1, kernel)
         image = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 13, 2)
         image = cv.medianBlur(image, 5)
         h, w = image.shape
         if h > w:
             image = cv.rotate(image, cv.ROTATE_90_COUNTERCLOCKWISE)
+            org = cv.rotate(org, cv.ROTATE_90_COUNTERCLOCKWISE) 
         if cv.getTrackbarPos("filter", "image"):
             cv.imshow("image", org)
         else:
@@ -48,4 +47,8 @@ def show():
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
+
+cv.namedWindow('image', flags=cv.WINDOW_NORMAL)
+cv.resizeWindow('image', 1800, 1100)
+cv.createTrackbar("filter", "image", 0, 1, change)
 show()
--- wbs_camera.py
+++ wbs_camera.py
@@ -8,10 +8,18 @@ def change():
 
 
 cap = cv.VideoCapture(int(sys.argv[1]))
-cv.namedWindow('image',flags=cv.WINDOW_NORMAL)
-cv.resizeWindow('image',1800,1100)
-cv.createTrackbar("filter", "image", 0, 1, change)
+# cap.set(cv.CAP_PROP_FRAME_WIDTH, 3840)
+# cap.set(cv.CAP_PROP_FRAME_HEIGHT, 2160)
+# cap.set(cv.CAP_PROP_FRAME_WIDTH, 2592)
+# cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1944)
+#cap.set(cv.CAP_PROP_FRAME_WIDTH, 1500)
+#cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1500)
+# cap.set(cv.CAP_PROP_BRIGHTNESS, 75)
 
+
+cv.namedWindow('image', flags=cv.WINDOW_NORMAL)
+cv.resizeWindow('image', 1800, 1100)
+cv.createTrackbar("filter", "image", 0, 1, change)
 
 while 1:
     # Capture frame-by-frame
@@ -20,7 +28,7 @@ while 1:
 
     kernel = np.ones((5, 5), np.float32) / 25
     image = cv.filter2D(image, -1, kernel)
-    image = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 13, 2)
+    image = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
     image = cv.medianBlur(image, 5)
     h, w = image.shape
     if h > w:
